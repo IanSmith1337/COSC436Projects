e55c428ea25472cac9b7a40915b9ee2f6dc27c39
Trian <iansmith1337@gmail.com>
2020-08-27 15:37:15 -0400

    Starting!

--- Pair-Programming/PP.java
+++ Pair-Programming/PP.java
@@ -11,6 +11,15 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9, return [0, 1].
 */
     public static void main(String[] args) {
-        
+        Integer[] arrayIntegers = {1, 2, 3, 4};
+        Integer target = Integer.parseInt(args[0]);
+        for (int i = 0; i < arrayIntegers.length; i++) {
+            if(arrayIntegers[i] < target) {
+                
+            }
+            for (int j = 0; j < arrayIntegers.length; j++) {
+                System.out.println(arrayIntegers[i]);
+            }
+        }
     }
 }
