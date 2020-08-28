fc1ab641dc0e87d001d41587d831c84da3289f81
Trian <iansmith1337@gmail.com>
2020-08-27 22:20:29 -0400

    Finished Problem 2 and the Questions.

--- Pair-Programming/Problem2.java
+++ Pair-Programming/Problem2.java
@@ -7,27 +7,28 @@ Input: ["flower", "flow", "flight"]	Output: "fl"
 Input: ["dog", "racecar", "car"]		Output: ""
 Explanation: There is no common prefix among the input strings.
 */
+    public static void main(String[] args) {
+        String [] words = {"flower", "flow", "flight"};
+        System.out.println(commonPrefix(words));
+    }
 
-
-
-
-
-
+    public static String commonPrefix(String[] arr) {
+        if (arr.length == 0){
+            return "";
+        }
+        String temp = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            while (arr[i].indexOf(temp)!=0) {
+                temp = temp.substring(0, temp.length()-1);
+                if (temp.isEmpty()) {
+                    return "";
+                }
+            }    
+        } 
+        return temp;
+    }
 }
 
-
-
-/* 
- public String longestCommonPrefix(String[] strs) {
-    if (strs.length == 0) return "";
-    String prefix = strs[0];
-    for (int i = 1; i < strs.length; i++)
-        while (strs[i].indexOf(prefix) != 0) {
-            prefix = prefix.substring(0, prefix.length() - 1);
-            if (prefix.isEmpty()) return "";
-        }        
-    return prefix;
-}*/
 
 
 
