f4794ea6137970fdf7b541309afb93653cbba0bf
Trian <iansmith1337@gmail.com>
2020-09-10 15:56:03 -0400

    Deep and Driver finished

--- DeepCopy/Deep.java
+++ DeepCopy/Deep.java
@@ -1,5 +1,19 @@
 package DeepCopy;
 
+import java.util.Arrays;
+
 public class Deep {
-    
+    private int[] data;
+
+    public Deep(int[] values) {
+        data = new int [values.length];
+        for (int i = 0; i < data.length; i++) {
+            data[i]
+        }
+    }
+
+    public void showData()
+    {
+        System.out.println(Arrays.toString(data));
+    }
 }
--- DeepCopy/Driver.java
+++ DeepCopy/Driver.java
@@ -4,8 +4,20 @@ public class Driver {
     public static void main(String[] args) {
         int[] nums = {3, 7, 9};
         Shallow s = new Shallow(nums);
+        Deep d = new Deep(nums);
+        System.out.println("Shallow (Before Modification):");
         s.showData();
+        System.out.println("\n");
+        System.out.println("Deep (Before Modification):");
+        d.showData();
+        System.out.println("\n");
+    
         nums[0] = 13;
+        System.out.println("Shallow (After Modification)");
         s.showData();
+        System.out.println("\n");
+        System.out.println("Deep (After Modification):");
+        d.showData();
+        System.out.println("\n");
     }
 }
