592f3a9e36974542a6b07d31aa510869bd4741bf
Trian <iansmith1337@gmail.com>
2020-09-10 16:10:23 -0400

    ICE Completed

--- DeepCopy/Deep.java
+++ DeepCopy/Deep.java
@@ -6,7 +6,7 @@ public class Deep {
     private int[] data;
 
     public Deep(int[] values) {
-        data = new int [values.length];
+        data = new int[values.length];
         for (int i = 0; i < data.length; i++) {
             data[i] = values[i];
         }
