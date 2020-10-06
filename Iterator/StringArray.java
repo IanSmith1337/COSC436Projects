b3612bec6b9cd811b5c67c4984e35391bc7ff402
Trian <iansmith1337@gmail.com>
2020-10-06 15:47:30 -0400

    StringArray Progress

---
+++ Iterator/StringArray.java
@@ -0,0 +1,19 @@
+package Iterator;
+
+import java.util.*;
+
+public class StringArray implements Iterable<String> {
+    String[] values;
+    public StringArray(String[] values){
+        this.values = values;
+    }
+
+    public Iterator<String> iterator() {
+        return this.iterator();
+    }
+
+    public class ArrayIterator {
+        int current;
+        
+    }
+}
