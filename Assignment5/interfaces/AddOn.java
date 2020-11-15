8fa3bccac2a345d73a74939cd7ef85bc705695c7
Trian <iansmith1337@gmail.com>
2020-11-15 16:06:53 -0500

    GEETHOOB

---
+++ Assignment5/interfaces/AddOn.java
@@ -0,0 +1,6 @@
+package Assignment5.interfaces;
+
+public public interface AddOn {   // the type of added info to a BasicReceipt (e.g., greetings, rebates, coupons)
+	public boolean applies(PurchasedItems items);
+	public String getLines();
+}
