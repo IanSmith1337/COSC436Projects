c3ae1d771ef4688771a6723ecb593803cc277375
Trian <iansmith1337@gmail.com>
2020-11-24 15:40:40 -0500

    .

--- Assignment6/UI.java
+++ Assignment6/UI.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 
 public class UI {
         // TODO: The user interface should just be a text-based numbered list of options, implemented in the main method. (It can be a GUI if you desire and are familiar with the development of GUIs, but no extra points will be given for this).        
-    public static void main(String args[]){
+    public static void main(String[] args){
        JFrame frame = new JFrame("Ordering Client V1.0");
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        frame.setSize(300,300);
